1afd04db5107e7a707b5adcc9d1dca1dfe3cb5ec
unknown <shirvani.soheil647@gmail.com>
2022-04-11 18:22:28 -0300

    SMTP Credintials Fixed

--- MyFunProject/settings.py
+++ MyFunProject/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shirvani.soheil647@gmail.com'
-EMAIL_HOST_PASSWORD = 'yvxbbyveivdsfdlc'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
